ed4ff031f1ea24e30143ecb14644a5e9fd47d3f7
Z-DEREN <zhangderen233@gmail.com>
2020-04-05 11:11:27 +0800

    创建项目

---
+++ whim_laughable/src/main/java/com/zdr/whim_laughable/WhimLaughableApplication.java
@@ -0,0 +1,13 @@
+package com.zdr.whim_laughable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhimLaughableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhimLaughableApplication.class, args);
+    }
+
+}
---
+++ whim_laughable/src/test/java/com/zdr/whim_laughable/WhimLaughableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdr.whim_laughable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhimLaughableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
