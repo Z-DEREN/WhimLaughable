37bd40257d05f4ed970d8337b7236cec3585e371
Z-DEREN <zhangderen233@gmail.com>
2020-04-07 18:12:28 +0800

    感谢程序猿张先森的开源的一整套博客项目 这是他的博客主页https://www.zhyocean.cn/ 目前在啃他的博客源码,并将部分代码拷到我的项目中借鉴并学习

--- whim_laughable/src/main/java/com/zdr/whim_laughable/Main/Controller/Home.java
+++ whim_laughable/src/main/java/com/zdr/whim_laughable/Main/Controller/Home.java
@@ -6,27 +6,77 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @Controller
-@ResponseBody
-@RequestMapping("/whim_laughable")
 public class Home {
 
     @Autowired
     private Tool tool;
+
     /**
      * 主页导航页
      * @param model
      * @return
      */
-    @RequestMapping(value = "/home1", method = RequestMethod.GET)
+    @RequestMapping(value = "/")
+    public Object Hpublicity(ModelAndView model , HttpServletRequest request) {
+        String lastUrl = request.getHeader("Referer");
+        System.out.println("进入系统"+lastUrl);
+        model.addObject("Time", tool.GetNewDateTime(2));
+        model.setViewName("administrator/he/Hpublicity");
+        return model;
+    }
+
+    /**
+     * 主页导航页
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/creationessay")
+    public Object creationessay(ModelAndView model , HttpServletRequest request) {
+        String lastUrl = request.getHeader("Referer");
+        System.out.println("进入写博客"+lastUrl);
+        model.addObject("Time", tool.GetNewDateTime(2));
+        model.setViewName("administrator/universal/Rich_text_editor/DemoText");
+        return model;
+    }
+
+
+    /**
+     * 主页导航页
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/managementessay")
+    public Object managementessay(ModelAndView model , HttpServletRequest request) {
+        String lastUrl = request.getHeader("Referer");
+        System.out.println("管理博客"+lastUrl);
+        model.addObject("Time", tool.GetNewDateTime(2));
+        model.setViewName("administrator/I/managementessay");
+        return model;
+    }
+
+
+
+
+
+
+
+
+
+
+    /**
+     * 主页导航页
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "/home1")
     public Object home1(ModelAndView model) {
         System.out.println("进入系统");
-
         model.addObject("Time", tool.GetNewDateTime(2));
-        model.setViewName("administrator/universal/Rich_text_editor/DemoTextStyle");
+        model.setViewName("administrator/universal/button/cybertruckButton");
         return model;
     }
 
